5f8ecede8ef921504c4bd33a6513f3deeab9d103
Valentin Marta <valenmarta17@gmail.com>
2024-03-06 20:38:21 -0300

    I create the menu of the virtual movie manager

---
+++ main.py
@@ -0,0 +1,32 @@
+def main():
+
+    while True:
+
+        # Options for the user
+        op = input("""Welcolme to our virtual movie manager, please select one option:
+        1. Create movie.
+        2. Verify if the movie is already on the list.
+        3. Ask for all the movies of one specific year.
+        4. Present one movie of the list.
+        5. Change the type of the movie.
+        6. Exit.""")
+
+        # Menu with functions
+        if op == "1":
+            pass
+        elif op == "2":
+            pass
+        elif op == "3":
+            pass
+        elif op == "4":
+            pass
+        elif op == "5":
+            pass
+        elif op == "6":
+            print("Thank you for using ous service")
+            break
+        else:
+            print("invalid value, try again")
+
+if __name__ == "__main__":
+    main()
